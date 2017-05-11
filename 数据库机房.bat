bbf287cbe5e1f522a5a5fd6ef41598259a1acf61
houchuanhao <houchuanhao@imudges.com>
2017-05-11 13:18:31 +0800

    添加了数据库机房.bat

---
+++ 数据库机房.bat
@@ -0,0 +1,3 @@
+netsh interface ip set address name="ÒÔÌ«Íø" source=static addr=183.175.10.116 mask=255.255.255.0 gateway=183.175.10.65
+netsh interface ip set dns name="ÒÔÌ«Íø" source=static addr=202.207.0.6
+pause
